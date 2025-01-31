ff510ac91fa4782a9dd6a6a0e09166d1dbd9647c
njordan41 <nicole.jordan284@gmail.com>
2025-01-30 19:15:35 -0500

    set up

---
+++ README.md
@@ -0,0 +1,3 @@
+# Final Project - Nicole Jordan
+
+This is the final project for my General Assembly Front End Web Development Class.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+const App = () => {
+  return <></>
+}
+
+export default App
+
+// may need parenthesis after return and before the last yellow curly bracket
